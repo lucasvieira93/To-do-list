70a4ca09a7c153ba9c7cb2cd1e3a476a4a54425b
Lucas Vieira <lvieirastos@gmail.com>
2022-03-10 18:15:30 -0300

    add: activity_add_task.xml

--- app/src/main/java/com/lucasvieira/todolist/MainActivity.kt
+++ app/src/main/java/com/lucasvieira/todolist/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.lucasvieira.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab_tarefa.setOnClickListener {}
     }
 }
