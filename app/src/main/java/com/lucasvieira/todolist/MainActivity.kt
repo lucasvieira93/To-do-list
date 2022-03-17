0826ceb2883585d362e70043a234f3be1339e186
Lucas Vieira <lvieirastos@gmail.com>
2022-03-17 00:14:15 -0300

    add: listview e recuperação dos dados de outra tela

--- app/src/main/java/com/lucasvieira/todolist/MainActivity.kt
+++ app/src/main/java/com/lucasvieira/todolist/MainActivity.kt
@@ -3,13 +3,34 @@ package com.lucasvieira.todolist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fab_tarefa.setOnClickListener {}
+        //Pegar dados da tela anterior - TODO(reason = "A lista só retorna o ultimo valor repetidamente")
+
+        var bundle: Bundle? = intent.extras
+        if (bundle != null) {
+            var message = intent.getStringExtra("titulo").toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+        //lista Array para mostrar - TODO(reason = "A lista só retorna o ultimo valor repetidamente")
+
+        val dados = Array(3) { "Lucas"; "Matheus"; "Junior" }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
+        listview_tarefas.adapter = adapter
+
+        //ação botão FAB
+        fab_tarefa.setOnClickListener {
+            val intent = Intent(this, AdicionarActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
