82a94ddb3118e53acaf346d8ce973f8d38bcc35f
Lucas Vieira <lvieirastos@gmail.com>
2022-03-18 19:06:22 -0300

    feat: ação botão cancelar

--- app/src/main/java/com/lucasvieira/todolist/AdicionarActivity.kt
+++ app/src/main/java/com/lucasvieira/todolist/AdicionarActivity.kt
@@ -12,9 +12,15 @@ class AdicionarActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_task)
 
         btn_new_task.setOnClickListener {
+            val tarefa_titulo = edit_text_titulo.text.toString()
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("titulo", edit_text_titulo.text.toString())
-            Toast.makeText(this, "Tarefa adicionada!", Toast.LENGTH_SHORT).show()
+            intent.putExtra("titulo", tarefa_titulo)
+            Toast.makeText(this, "Tarefa ${tarefa_titulo} foi adicionada!", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+
+        btn_cancel.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/lucasvieira/todolist/MainActivity.kt
+++ app/src/main/java/com/lucasvieira/todolist/MainActivity.kt
@@ -34,6 +34,4 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
-
 }
